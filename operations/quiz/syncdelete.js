9aa8567012a7d45db7e0f5a1a30cac04eaee0331
shawn <shawn310480@gmail.com>
2024-02-21 18:31:25 -0500

    022124

--- operations/quiz/syncdelete.js
+++ operations/quiz/syncdelete.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+
 fs.readFile("./operations/data.txt", (err, data) => {
   if (err) console.log(err.message);
-  else console.log(data);
-});
-fs.unlinkSync("./operations/data.txt");		// delete the file
+  else {
+    fs.unlinkSync("./operations/data.txt", (err, data) => {
+      console.log('file deleted', data, err);
+    })
+  }
+})
 
